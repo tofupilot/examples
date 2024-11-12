043ccaef5b2b1f13a0e33a9fe165b479357dc7f9
Felix <felix.berthier@epita.fr>
2024-11-12 14:18:53 +0100

    Inlined openhtf script for now

--- api/index.py
+++ api/index.py
@@ -1,36 +1,33 @@
-import sys
-import subprocess
 from http.server import BaseHTTPRequestHandler
+
+import openhtf as htf
+from openhtf.util import units
+from tofupilot.openhtf import TofuPilot
+
+
+def phase_one(test):
+    return htf.PhaseResult.CONTINUE
+
+
+@htf.measures(
+    htf.Measurement("temperature").in_range(0, 100).with_units(units.DEGREE_CELSIUS)
+)
+def phase_temperature(test):
+    test.measurements.temperature = 25
+
+
+def main():
+    test = htf.Test(phase_one, phase_temperature)
+    with TofuPilot(test):  # just works
+        test.execute(lambda: "PCB001")
 
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        try:
-            # Use the absolute path to the Python interpreter
-            python_path = sys.executable
-            result = subprocess.run(
-                [python_path, "./templates/measurements.py"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True,
-                check=False,
-            )
-
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-
-            if result.returncode == 0:
-                response_message = f"Script Output:\n{result.stdout}"
-            else:
-                response_message = (
-                    f"Script encountered an error (Exit code {result.returncode}):\n"
-                    f"{result.stderr}"
-                )
-            self.wfile.write(response_message.encode("utf-8"))
-        except Exception as e:
-            self.send_response(500)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            error_message = f"Error executing script: {str(e)}"
-            self.wfile.write(error_message.encode("utf-8"))
+        main()
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Hello, world!".encode("utf-8"))
+        return
