61c8e3c35ec62457d55445809dcff0888013af97
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2025-01-24 14:54:16 +0100

    Add phases openhtf scripts

---
+++ docs/openhtf/phases/phases-required/main.py
@@ -0,0 +1,19 @@
+import openhtf as htf
+from tofupilot.openhtf import TofuPilot
+
+
+def phase_one(test):  # Phase name is the function name
+    return htf.PhaseResult.CONTINUE  # Pass status
+
+
+def main():
+    test = htf.Test(phase_one, procedure_id="FVT1", part_number="PCB1")
+
+    with TofuPilot(test):
+        test.execute(
+            lambda: "PCB1A001"
+        )  # duration and start time are automatically set
+
+
+if __name__ == "__main__":
+    main()
