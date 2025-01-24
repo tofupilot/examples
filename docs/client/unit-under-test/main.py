8122bebf2ff80654d69546184a8012a36bcfdccf
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2025-01-24 14:49:51 +0100

    Add main to unit-under-test python script

--- docs/client/unit-under-test/main.py
+++ docs/client/unit-under-test/main.py
@@ -1,14 +1,20 @@
 from tofupilot import TofuPilotClient
 
-client = TofuPilotClient()
 
-client.create_run(
-    procedure_id="FVT1",
-    unit_under_test={
-        "serial_number": "PCB1A001",
-        "part_number": "PCB1",
-        "revision": "A",  # optional
-        "batch_number": "12-24",  # optional
-    },
-    run_passed=True,
-)
+def main():
+    client = TofuPilotClient()
+
+    client.create_run(
+        procedure_id="FVT1",
+        unit_under_test={
+            "serial_number": "PCB1A001",
+            "part_number": "PCB1",
+            "revision": "A",  # optional
+            "batch_number": "12-24",  # optional
+        },
+        run_passed=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
