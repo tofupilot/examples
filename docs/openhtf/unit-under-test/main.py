82c4710ac91881ac3d534e233535f6f27cd550f7
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2025-01-24 14:50:06 +0100

    Add procedure id to unit under test openhtf script

--- docs/openhtf/unit-under-test/main.py
+++ docs/openhtf/unit-under-test/main.py
@@ -6,6 +6,7 @@ def main():
     phases = []  # Your test phases here
     test = Test(
         phases,
+        procedure_id="FVT1",
         part_number="PCB1",
         revision="A",  # optional
         batch_number="12-24",  # optional
