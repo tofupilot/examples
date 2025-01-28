47a026b744f4b5b4537d078fb62b71ef42d5985c
Felix <felix.berthier@epita.fr>
2025-01-28 11:35:37 +0100

    Added API calls to example repository

---
+++ api/index.py
@@ -0,0 +1,70 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import get_started.scripts.client as client
+import get_started.scripts.openhtf as openhtf
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_OPTIONS(self):
+        # Handling preflight requests for CORS
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+        self.end_headers()
+
+    def do_POST(self):
+        auth_header = self.headers.get("Authorization")
+        if not auth_header:
+            self.send_response(401)
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+            self.send_header(
+                "Access-Control-Allow-Headers", "Authorization, Content-Type"
+            )
+            self.end_headers()
+            self.wfile.write(b"Missing Authorization header")
+            return
+
+        try:
+            _, api_key = auth_header.split(" ")
+        except ValueError:
+            self.send_response(400)
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+            self.send_header(
+                "Access-Control-Allow-Headers", "Authorization, Content-Type"
+            )
+            self.end_headers()
+            self.wfile.write(b"Invalid Authorization header format")
+            return
+
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(content_length).decode("utf-8")
+
+        # Attempting to parse JSON body
+        try:
+            body_data = json.loads(body) if body else {}
+        except json.JSONDecodeError:
+            body_data = {}
+
+        url = body_data.get("url", None)
+        framework = body_data.get("framework", "openhtf")
+
+        # Calling the appropriate function based on the framework
+        if framework == "client":
+            client.simple(api_key, url)
+        elif framework == "openhtf":
+            openhtf.simple(api_key, url)
+
+        # Sending a successful response
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+        self.end_headers()
+
+        response_body = {"message": "Request processed successfully"}
+        self.wfile.write(json.dumps(response_body).encode("utf-8"))
---
+++ get_started/scripts/openhtf.py
@@ -0,0 +1,23 @@
+"""
+Example demonstrating how to create a test run using OpenHTF.
+
+This script initializes a test run for a unit with the serial number "00102" and part number "PCB01".
+The test run is marked as successful (passed) with a single step.
+"""
+
+from typing import Optional
+
+import openhtf as htf
+from tofupilot.openhtf import TofuPilot
+
+
+# Create a successful test step
+def step_one():
+    return htf.PhaseResult.CONTINUE
+
+
+def simple(api_key: Optional[str], url: Optional[str]):
+    # Set up test run for unit "00102" adding a single line before test execution
+    test = htf.Test(step_one, procedure_id="FVT1", part_number="PCB01")
+    with TofuPilot(test, api_key=api_key, url=url, stream=False):
+        test.execute(lambda: "00102")
