26f8bdd1c16b29cf3ff5fe57c0ce76c04f852d66
upview <upview@users.noreply.github.com>
2025-01-31 14:00:03 +0000

    Auto code format

--- welcome_aboard/openhtf/main.py
+++ welcome_aboard/openhtf/main.py
@@ -1,7 +1,9 @@
-from tofupilot.openhtf import TofuPilot
+import random
+
 import openhtf as htf
 from openhtf.util import units
-import random
+from tofupilot.openhtf import TofuPilot
+
 
 @htf.measures(
     htf.Measurement("mcu_core_voltage").in_range(1.7, 1.9).with_units(units.VOLT),
@@ -13,29 +15,36 @@ def check_mcu_power(test):
     test.measurements.mcu_clock_speed = 48
     test.measurements.flash_integrity = True
 
+
 @htf.measures(
     htf.Measurement("sensor_i2c_response").equals(True),
-    htf.Measurement("sensor_adc_accuracy").in_range(0.95, 1.05).with_units(units.PERCENT),
-    htf.Measurement("sensor_temperature_reading").in_range(-10, 85).with_units(units.CELSIUS),
+    htf.Measurement("sensor_adc_accuracy")
+    .in_range(0.95, 1.05)
+    .with_units(units.PERCENT),
+    htf.Measurement("sensor_temperature_reading")
+    .in_range(-10, 85)
+    .with_units(units.CELSIUS),
 )
 def check_sensors(test):
     test.measurements.sensor_i2c_response = True
-    test.measurements.sensor_adc_accuracy = round(random.uniform(0.95, 1.05), 2)
-    test.measurements.sensor_temperature_reading = round(random.uniform(-10, 85), 1)
+    test.measurements.sensor_adc_accuracy = round(
+        random.uniform(0.95, 1.05), 2)
+    test.measurements.sensor_temperature_reading = round(
+        random.uniform(-10, 85), 1)
+
 
 def generate_serial_number():
     random_digits = "".join([str(random.randint(0, 9)) for _ in range(3)])
     return f"PCBA123{random_digits}"
 
+
 def main():
     test = htf.Test(
         check_mcu_power,
         check_sensors,
-
         # Procedure information
         test_name="PCBA Functional Testing",
         procedure_id="PCBA_FVT1",  # optional
-        
         # Unit Under Test information
         part_number="PCBA123",
         part_name="PCBA Control Board",  # optional
@@ -45,6 +54,7 @@ def main():
 
     with TofuPilot(test):
         test.execute(lambda: generate_serial_number())
+
 
 if __name__ == "__main__":
     main()
