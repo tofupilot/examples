e95301b414a644e47b387211a54971f2fff9c8c3
Julien Buteau <julien.buteau@gmail.com>
2025-02-02 21:11:27 +0100

    Disabled preview mode of black

--- welcome_aboard/openhtf/main.py
+++ welcome_aboard/openhtf/main.py
@@ -18,13 +18,13 @@ def check_mcu_power(test):
 
 @htf.measures(
     htf.Measurement("i2c_response").equals(True),
-    htf.Measurement("adc_accuracy").in_range(0.95, 1.05).with_units(units.PERCENT),
-    htf.Measurement("temperature_value") .in_range(-10, 85) .with_units(units.DEGREE_CELSIUS),
+    htf.Measurement("accuracy").in_range(0.95, 1.05).with_units(units.PERCENT),
+    htf.Measurement("temperature").in_range(-10, 85).with_units(units.DEGREE_CELSIUS),
 )
 def check_sensors(test):
     test.measurements.i2c_response = True
-    test.measurements.adc_accuracy = round(random.uniform(0.95, 1.05), 2)
-    test.measurements.temperature_value = round(random.uniform(-10, 85), 1)
+    test.measurements.accuracy = round(random.uniform(0.95, 1.05), 2)
+    test.measurements.temperature = round(random.uniform(-10, 85), 1)
 
 
 def main(serial_number: str):
