1e63026b9118ecf9e90ee6a58f71b87e452669c9
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2025-02-17 18:26:20 +0100

    Clean started_at script in QA

--- qa/client/create_run/started_at/main.py
+++ qa/client/create_run/started_at/main.py
@@ -25,36 +25,6 @@ def check_voltage():
     return passed, value_measured, "V", None, 13
 
 
-def check_soc():
-    passed = simulate_test_result(95)
-    value_measured = (
-        round(
-            random.uniform(
-                50,
-                55),
-            0) if passed else round(
-            random.uniform(
-                20,
-                39),
-            2))
-    return passed, value_measured, "%", 40, 60
-
-
-def check_soh():
-    passed = simulate_test_result(100)
-    value_measured = (
-        round(random.uniform(96, 100), 0)
-        if passed
-        else round(random.uniform(70, 95), 2)
-    )
-    return passed, value_measured, "%", 95, None
-
-
-def take_picture():
-    passed = simulate_test_result(100)
-    return passed, None, None, None, None
-
-
 def flash_firmware():
     passed = simulate_test_result(90)
     value_measured = "1.2.4" if passed else "1.2.0"
@@ -64,7 +34,6 @@ def flash_firmware():
 # Running Tests
 def run_test(test, duration):
     start_time = datetime.now() - timedelta(days=1)
-    start_time = datetime.now()
     passed, value_measured, unit, limit_low, limit_high = test()
 
     step = {
@@ -85,9 +54,6 @@ def run_all_tests():
         (flash_firmware, timedelta(minutes=3, seconds=32)),
         (battery_connection, timedelta(seconds=8)),
         (check_voltage, timedelta(seconds=3)),
-        (check_soc, timedelta(seconds=1)),
-        (check_soh, timedelta(seconds=5)),
-        (take_picture, timedelta(seconds=12)),
     ]
 
     steps = []
@@ -103,7 +69,7 @@ def run_all_tests():
 
 def handle_test():
     random_digits = "".join([str(random.randint(0, 9)) for _ in range(5)])
-    serial_number = f"SI0364A{random_digits}"
+    serial_number = f"SI0364B{random_digits}"
 
     # Exécuter tous les tests
     steps, total_duration = run_all_tests()
@@ -111,11 +77,10 @@ def handle_test():
     # Créer un rapport de test
     client.create_run(
         procedure_id="FVT9",
-        # procedure_name="olé",
         unit_under_test={
-            "part_number": "SI03ddd64A",
-            "revision": "3.1",
-            "batch_number": "12-24",
+            "part_number": "SI0364",
+            "revision": "B",
+            "batch_number": "01-25",
             "serial_number": serial_number,
         },
         run_passed=all(step["step_passed"] for step in steps),
