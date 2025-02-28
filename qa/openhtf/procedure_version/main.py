0a49eff091fc93ca3dc8fe1f2c4d4a4d00f8d330
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2025-02-28 15:11:26 +0100

    Updating procedure in procedure_version test

--- qa/openhtf/procedure_version/main.py
+++ qa/openhtf/procedure_version/main.py
@@ -14,9 +14,9 @@ def check_button(test):
 def main():
     test = htf.Test(
         check_button,
-        # procedure_id="FVT13", # No need to specify the ID
-        procedure_name="PCBA Testing OpenHTF",  # Create procedure name
-        procedure_version="1.2.18",  # Create procedure version
+        procedure_id="FVT1",  # No need to specify the ID
+        procedure_name="PCB Testing",  # Create procedure name
+        procedure_version="1.2.19",  # Create procedure version
         part_number="00220",
         revision="B",
     )
